e1632cee18735dd46fbc7d79a8ab130e791c26a1
John Slavick <john.slavick@gmail.com>
2015-12-27 20:43:10 -0600

    The Food Types and Serving Sizes lists are now fully expanded.

--- app/src/main/java/org/slavick/dailydozen/activity/FoodInfoActivity.java
+++ app/src/main/java/org/slavick/dailydozen/activity/FoodInfoActivity.java
@@ -3,7 +3,11 @@ package org.slavick.dailydozen.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import org.slavick.dailydozen.Args;
@@ -22,8 +26,6 @@ public class FoodInfoActivity extends AppCompatActivity {
     @Bind(R.id.food_serving_sizes)
     protected ListView lvFoodServingSizes;
 
-    private String foodName;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,15 +38,35 @@ public class FoodInfoActivity extends AppCompatActivity {
     private void displayInfoForFood() {
         final Intent intent = getIntent();
         if (intent != null && intent.hasExtra(Args.FOOD_NAME)) {
-            foodName = intent.getStringExtra(Args.FOOD_NAME);
+            final String foodName = intent.getStringExtra(Args.FOOD_NAME);
 
-            lvFoodTypes.setAdapter(createAdapter(FoodInfo.getTypesOfFood(foodName)));
-
-            lvFoodServingSizes.setAdapter(createAdapter(FoodInfo.getServingSizes(foodName)));
+            initList(lvFoodTypes, FoodInfo.getTypesOfFood(foodName));
+            initList(lvFoodServingSizes, FoodInfo.getServingSizes(foodName));
         }
+    }
+
+    private void initList(final ListView listView, final List<String> items) {
+        listView.setAdapter(createAdapter(items));
+        fullyExpandList(listView);
     }
 
     private ArrayAdapter<String> createAdapter(final List<String> items) {
         return new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+    }
+
+    public void fullyExpandList(final ListView list) {
+        list.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getListViewHeight(list)));
+    }
+
+    private int getListViewHeight(final ListView list) {
+        final Adapter adapter = list.getAdapter();
+        final int count = adapter.getCount();
+
+        list.measure(
+                View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+
+        // The (count - 1) hides the final list item divider
+        return list.getMeasuredHeight() * count + ((count - 1) * list.getDividerHeight());
     }
 }
