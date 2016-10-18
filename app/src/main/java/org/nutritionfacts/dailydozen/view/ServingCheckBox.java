af289114444e31ecb8f51c86439b7563d5c45584
John Slavick <john.slavick@gmail.com>
2016-10-18 12:08:33 -0500

    Update ServingCheckBox to match code style

--- app/src/main/java/org/nutritionfacts/dailydozen/view/ServingCheckBox.java
+++ app/src/main/java/org/nutritionfacts/dailydozen/view/ServingCheckBox.java
@@ -2,38 +2,37 @@ package org.nutritionfacts.dailydozen.view;
 
 import android.content.Context;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
 public class ServingCheckBox extends CheckBox {
-
     private ServingCheckBox scbNextServing;
     private ServingCheckBox scvPrevServing;
-    private CompoundButton.OnCheckedChangeListener onCheckedChangeListener;
 
     public ServingCheckBox(Context context) {
         super(context);
     }
 
-    public void setNextServing(ServingCheckBox nextServing){
+    public void setNextServing(ServingCheckBox nextServing) {
         this.scvPrevServing = nextServing;
         nextServing.scbNextServing = this;
     }
 
-    public void onCheckChange(boolean isChecked){
-        if (isChecked)
+    public void onCheckChange(boolean isChecked) {
+        if (isChecked) {
             continueCheck();
-        else
+        } else {
             continueUncheck();
+        }
     }
 
-    private void continueCheck(){
-        if (scbNextServing != null && !scbNextServing.isChecked())
+    private void continueCheck() {
+        if (scbNextServing != null && !scbNextServing.isChecked()) {
             scbNextServing.setChecked(true);
+        }
     }
 
-    private void continueUncheck(){
-        if (scvPrevServing != null && scvPrevServing.isChecked())
+    private void continueUncheck() {
+        if (scvPrevServing != null && scvPrevServing.isChecked()) {
             scvPrevServing.setChecked(false);
+        }
     }
-
 }
