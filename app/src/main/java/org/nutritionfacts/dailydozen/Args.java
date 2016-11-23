a663052d583d24e19f04a061f523a07ca1f0dfd9
notmypassword <notmypassword>
2016-11-22 20:05:05 -0600

    Removing unused strings

--- app/src/main/java/org/nutritionfacts/dailydozen/Args.java
+++ app/src/main/java/org/nutritionfacts/dailydozen/Args.java
@@ -2,9 +2,6 @@ package org.nutritionfacts.dailydozen;
 
 public class Args {
     public static final String DAY = "day";
-    public static final String MONTH = "month";
-    public static final String YEAR = "year";
-    public static final String TODAY = "day";
     public static final String DATE = "date";
     public static final String DATES_WITH_EVENTS = "dates_with_events";
     public static final String FOOD_ID = "food_id";
