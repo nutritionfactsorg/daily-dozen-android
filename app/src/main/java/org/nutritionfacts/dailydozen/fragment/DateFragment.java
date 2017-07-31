f1b2b3503671b50bfd8a1bdcc8c292861958a063
John Slavick <john.slavick@gmail.com>
2017-07-31 07:40:53 -0500

    Added a divider to inform users that checking vitamins does not update the daily servings count

--- app/src/main/java/org/nutritionfacts/dailydozen/fragment/DateFragment.java
+++ app/src/main/java/org/nutritionfacts/dailydozen/fragment/DateFragment.java
@@ -2,6 +2,7 @@ package org.nutritionfacts.dailydozen.fragment;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -24,6 +25,7 @@ import org.nutritionfacts.dailydozen.model.Food;
 import org.nutritionfacts.dailydozen.model.Servings;
 import org.nutritionfacts.dailydozen.widget.DateServings;
 import org.nutritionfacts.dailydozen.widget.FoodServings;
+import org.nutritionfacts.dailydozen.widget.VitaminDivider;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -90,10 +92,18 @@ public class DateFragment extends Fragment {
 
                 updateServingsCount();
 
+                final Context context = getContext();
+                boolean addedVitaminDivider = false;
+
                 for (Food food : Food.getAllFoods()) {
-                    final FoodServings foodServings = new FoodServings(getContext());
+                    final FoodServings foodServings = new FoodServings(context);
                     final boolean success = foodServings.setDateAndFood(day, food);
                     if (success) {
+                        if (Common.isVitamin(food) && !addedVitaminDivider) {
+                            vgFoodServings.addView(new VitaminDivider(context));
+                            addedVitaminDivider = true;
+                        }
+
                         vgFoodServings.addView(foodServings);
                         Bus.register(foodServings);
                     }
