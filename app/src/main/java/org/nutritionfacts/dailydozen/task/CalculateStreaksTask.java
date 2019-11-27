86a216ebca6527c4d2d8623d2b83de24b04df69a
John Slavick <john.slavick@gmail.com>
2019-11-27 10:23:48 -0600

    Updated the unused CalculateStreaksTask to also calculate tweak streaks

--- app/src/main/java/org/nutritionfacts/dailydozen/task/CalculateStreaksTask.java
+++ app/src/main/java/org/nutritionfacts/dailydozen/task/CalculateStreaksTask.java
@@ -4,11 +4,14 @@ import android.content.Context;
 
 import com.activeandroid.ActiveAndroid;
 
+import org.jetbrains.annotations.NotNull;
 import org.nutritionfacts.dailydozen.R;
 import org.nutritionfacts.dailydozen.controller.Bus;
 import org.nutritionfacts.dailydozen.model.DDServings;
 import org.nutritionfacts.dailydozen.model.Day;
 import org.nutritionfacts.dailydozen.model.Food;
+import org.nutritionfacts.dailydozen.model.Tweak;
+import org.nutritionfacts.dailydozen.model.TweakServings;
 
 import java.util.List;
 
@@ -21,7 +24,7 @@ public class CalculateStreaksTask extends TaskWithContext<Void, Integer, Boolean
     protected void onPreExecute() {
         super.onPreExecute();
 
-        if (DDServings.isEmpty()) {
+        if (DDServings.isEmpty() || TweakServings.isEmpty()) {
             progress.hide();
             cancel(true);
         } else {
@@ -38,6 +41,13 @@ public class CalculateStreaksTask extends TaskWithContext<Void, Integer, Boolean
 
         final List<Day> allDays = Day.getAllDays();
 
+        boolean calcFoodStreaksResult = calculateFoodStreaks(allDays);
+        boolean calcTweakStreaksResult = calculateTweakStreaks(allDays);
+        return calcFoodStreaksResult && calcTweakStreaksResult;
+    }
+
+    @NotNull
+    private Boolean calculateFoodStreaks(List<Day> allDays) {
         final List<Food> allFoods = Food.getAllFoods();
         final int numFoods = allFoods.size();
 
@@ -65,6 +75,40 @@ public class CalculateStreaksTask extends TaskWithContext<Void, Integer, Boolean
             }
 
             publishProgress(i + 1, numFoods);
+        }
+
+        return true;
+    }
+
+    @NotNull
+    private Boolean calculateTweakStreaks(List<Day> allDays) {
+        final List<Tweak> allTweaks = Tweak.getAllTweaks();
+        final int numTweaks = allTweaks.size();
+
+        for (int i = 0; i < numTweaks; i++) {
+            ActiveAndroid.beginTransaction();
+
+            Tweak tweak = allTweaks.get(i);
+
+            try {
+                for (Day day : allDays) {
+                    if (isCancelled()) {
+                        return false;
+                    }
+
+                    final TweakServings servingsOnDate = TweakServings.getByDateAndTweak(day, tweak);
+                    if (servingsOnDate != null) {
+                        servingsOnDate.recalculateStreak();
+                        servingsOnDate.save();
+                    }
+                }
+
+                ActiveAndroid.setTransactionSuccessful();
+            } finally {
+                ActiveAndroid.endTransaction();
+            }
+
+            publishProgress(i + 1, numTweaks);
         }
 
         return true;
