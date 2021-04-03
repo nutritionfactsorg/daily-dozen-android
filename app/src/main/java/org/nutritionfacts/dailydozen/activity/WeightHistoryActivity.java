cbc87530932fd8c3adfc4508fadf97ba40989d73
slavick <john.slavick@gmail.com>
2021-04-03 10:33:59 -0500

    Update WeightHistoryActivity to use View Binding

--- app/src/main/java/org/nutritionfacts/dailydozen/activity/WeightHistoryActivity.java
+++ app/src/main/java/org/nutritionfacts/dailydozen/activity/WeightHistoryActivity.java
@@ -15,8 +15,8 @@ import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import org.greenrobot.eventbus.Subscribe;
 import org.nutritionfacts.dailydozen.Args;
 import org.nutritionfacts.dailydozen.Common;
-import org.nutritionfacts.dailydozen.R;
 import org.nutritionfacts.dailydozen.controller.Bus;
+import org.nutritionfacts.dailydozen.databinding.ActivityServingsHistoryBinding;
 import org.nutritionfacts.dailydozen.event.LoadHistoryCompleteEvent;
 import org.nutritionfacts.dailydozen.event.TimeRangeSelectedEvent;
 import org.nutritionfacts.dailydozen.event.TimeScaleSelectedEvent;
@@ -24,33 +24,22 @@ import org.nutritionfacts.dailydozen.model.Day;
 import org.nutritionfacts.dailydozen.model.enums.TimeScale;
 import org.nutritionfacts.dailydozen.task.LoadWeightsHistoryTask;
 import org.nutritionfacts.dailydozen.task.params.LoadHistoryTaskParams;
-import org.nutritionfacts.dailydozen.view.TimeRangeSelector;
-import org.nutritionfacts.dailydozen.view.TimeScaleSelector;
 
 import java.util.Date;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class WeightHistoryActivity extends AppCompatActivity
         implements AdapterView.OnItemSelectedListener, OnChartValueSelectedListener {
-
-    @BindView(R.id.daily_servings_history_time_scale)
-    protected TimeScaleSelector timeScaleSelector;
-    @BindView(R.id.daily_servings_history_time_range)
-    protected TimeRangeSelector timeRangeSelector;
-    @BindView(R.id.daily_servings_chart)
-    protected CombinedChart chart;
+    private ActivityServingsHistoryBinding binding;
 
     private boolean alreadyLoadingData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_servings_history);
-        ButterKnife.bind(this);
+        binding = ActivityServingsHistoryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        timeScaleSelector.setVisibility(View.GONE);
+        binding.dailyServingsHistoryTimeScale.setVisibility(View.GONE);
         initTimeRangeSelector();
 
         loadData();
@@ -60,7 +49,7 @@ public class WeightHistoryActivity extends AppCompatActivity
         final Day firstDay = Day.getFirstDay();
         final Day lastDay = Day.getLastDay();
         if (firstDay != null && lastDay != null) {
-            timeRangeSelector.setStartAndEnd(
+            binding.dailyServingsHistoryTimeRange.setStartAndEnd(
                     firstDay.getYear(), firstDay.getMonth(),
                     lastDay.getYear(), lastDay.getMonth());
         } else {
@@ -72,14 +61,14 @@ public class WeightHistoryActivity extends AppCompatActivity
     protected void onResume() {
         super.onResume();
         Bus.register(this);
-        Bus.register(timeRangeSelector);
+        Bus.register(binding.dailyServingsHistoryTimeRange);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Bus.unregister(this);
-        Bus.unregister(timeRangeSelector);
+        Bus.unregister(binding.dailyServingsHistoryTimeRange);
     }
 
     private void loadData() {
@@ -88,8 +77,8 @@ public class WeightHistoryActivity extends AppCompatActivity
 
             new LoadWeightsHistoryTask(this).execute(new LoadHistoryTaskParams(
                     TimeScale.DAYS,
-                    timeRangeSelector.getSelectedYear(),
-                    timeRangeSelector.getSelectedMonth()));
+                    binding.dailyServingsHistoryTimeRange.getSelectedYear(),
+                    binding.dailyServingsHistoryTimeRange.getSelectedMonth()));
         }
     }
 
@@ -111,45 +100,45 @@ public class WeightHistoryActivity extends AppCompatActivity
             return;
         }
 
-        chart.setVisibility(View.VISIBLE);
+        binding.dailyServingsChart.setVisibility(View.VISIBLE);
 
-        chart.setData(chartData);
+        binding.dailyServingsChart.setData(chartData);
 
         // Draw bars behind lines
-        chart.setDrawOrder(new CombinedChart.DrawOrder[]{
+        binding.dailyServingsChart.setDrawOrder(new CombinedChart.DrawOrder[]{
                 CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE
         });
 
-        chart.setVisibleXRange(5, 5);
+        binding.dailyServingsChart.setVisibleXRange(5, 5);
 
-        chart.getXAxis().setDrawLabels(true);
+        binding.dailyServingsChart.getXAxis().setDrawLabels(true);
 
         // Without this line, MPAndroidChart v2.1.6 cuts off the tops of the X-axis date labels
-        chart.setExtraTopOffset(4f);
+        binding.dailyServingsChart.setExtraTopOffset(4f);
 
         // Start the chart with the latest day in view
-        chart.moveViewToX(chart.getXChartMax());
+        binding.dailyServingsChart.moveViewToX(binding.dailyServingsChart.getXChartMax());
 
-        chart.setDescription("");
+        binding.dailyServingsChart.setDescription("");
 
         // Prevents the value for each bar from drawing over the labels at the top
-        chart.setDrawValueAboveBar(false);
+        binding.dailyServingsChart.setDrawValueAboveBar(false);
 
-        chart.getAxisRight().setEnabled(false);
+        binding.dailyServingsChart.getAxisRight().setEnabled(false);
 
-        chart.getAxisLeft().setAxisMaxValue(event.getMaxVal() + 5);
-        chart.getAxisLeft().setAxisMinValue(event.getMinVal() - 5);
+        binding.dailyServingsChart.getAxisLeft().setAxisMaxValue(event.getMaxVal() + 5);
+        binding.dailyServingsChart.getAxisLeft().setAxisMinValue(event.getMinVal() - 5);
 
         // Disable all zooming and interacting with the chart
-        chart.setScaleEnabled(false);
-        chart.setPinchZoom(false);
-        chart.setDoubleTapToZoomEnabled(false);
-        chart.setHighlightPerDragEnabled(false);
+        binding.dailyServingsChart.setScaleEnabled(false);
+        binding.dailyServingsChart.setPinchZoom(false);
+        binding.dailyServingsChart.setDoubleTapToZoomEnabled(false);
+        binding.dailyServingsChart.setHighlightPerDragEnabled(false);
 
-        chart.setOnChartValueSelectedListener(this);
+        binding.dailyServingsChart.setOnChartValueSelectedListener(this);
 
         // Only enable jumping to dates if the user is viewing daily data
-        chart.setHighlightPerTapEnabled(event.getTimeScale() == TimeScale.DAYS);
+        binding.dailyServingsChart.setHighlightPerTapEnabled(event.getTimeScale() == TimeScale.DAYS);
 
         alreadyLoadingData = false;
     }
