ab9655527d5ec71ff51244cf85565807b9b338a6
slavick <john.slavick@gmail.com>
2021-10-02 17:03:25 -0500

    Revert "Reduce code duplication between pager adapters"
    This reverts commit 0c91bfc4

--- app/src/main/java/org/nutritionfacts/dailydozen/adapter/DailyDozenPagerAdapter.java
+++ app/src/main/java/org/nutritionfacts/dailydozen/adapter/DailyDozenPagerAdapter.java
@@ -1,20 +1,37 @@
 package org.nutritionfacts.dailydozen.adapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
 import org.nutritionfacts.dailydozen.fragment.DailyDozenFragment;
 import org.nutritionfacts.dailydozen.model.Day;
 
-public class DailyDozenPagerAdapter extends SynchronizedDatePagerAdapter {
+public class DailyDozenPagerAdapter extends FragmentStatePagerAdapter {
+    private int numDaysSinceEpoch;
+
     public DailyDozenPagerAdapter(@NonNull FragmentManager fm, int behavior) {
         super(fm, behavior);
+
+        this.numDaysSinceEpoch = Day.getNumDaysSinceEpoch();
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
         return DailyDozenFragment.newInstance(Day.getByOffsetFromEpoch(position));
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Day.getTabTitleForDay(position);
+    }
+
+    @Override
+    public int getCount() {
+        return numDaysSinceEpoch;
     }
 }
