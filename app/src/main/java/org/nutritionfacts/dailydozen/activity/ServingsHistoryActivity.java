deafed00c4d0554b94a042bff161d4288a70057c
John Slavick <slavick@users.noreply.github.com>
2025-10-28 06:35:32 -0500

    v28.1 (#266)
    * Translation update v06. Adds Hungarian_hu translation. (#262)
    
    * Update: v06 translations. Adds Hungarian_hu.
    
    * [FIX] Applies primary level Hungarian quotation marks „ (U+201E) ” (U+201D).
    
    * Adds Lychees video topic.
    
    * Hungarian update "CHECKED_Hungarian_hu.app.v06.DoubleCheck.tsv.txt"
    
    - https://github.com/nutritionfactsorg/daily-dozen-localization/issues/53#issuecomment-3258154735
    
    * Translation v06 testing updates (#264)
    
    * Quantity correction, all languages.
    
    - 60 g raw leafy vegetables (originally submitted as 90 g for v06)
    - 90 g raw or cooked non-leafy vegetables (originally submitted as 60 g for v06)
    
    * translation updates: bg, de, es, pt*, ru, zh*
    
    * Volunteer translator list update.
    
    * Fix some potential null pointer exceptions
    
    * Translation v06: Hungarian 20251020 update from beta testing. (#265)
    
    * Quantity correction, all languages.
    
    - 60 g raw leafy vegetables (originally submitted as 90 g for v06)
    - 90 g raw or cooked non-leafy vegetables (originally submitted as 60 g for v06)
    
    * translation updates: bg, de, es, pt*, ru, zh*
    
    * Volunteer translator list update.
    
    * Hungarian 20251020 update from beta testing.
    
    * Update gradle
    
    * Update dependencies
    
    * Update versionCode
    
    ---------
    
    Co-authored-by: --marc <marc-medley@users.noreply.github.com>

--- app/src/main/java/org/nutritionfacts/dailydozen/activity/ServingsHistoryActivity.java
+++ app/src/main/java/org/nutritionfacts/dailydozen/activity/ServingsHistoryActivity.java
@@ -73,8 +73,12 @@ public class ServingsHistoryActivity extends DailyDozenActivity
             alreadyLoadingData = true;
 
             binding.dailyServingsChart.setVisibility(View.GONE);
-            binding.dailyServingsLoadingLabel.setVisibility(View.VISIBLE);
-            binding.dailyServingsLoading.setVisibility(View.VISIBLE);
+            if (binding.dailyServingsLoadingLabel != null) {
+                binding.dailyServingsLoadingLabel.setVisibility(View.VISIBLE);
+            }
+            if (binding.dailyServingsLoading != null) {
+                binding.dailyServingsLoading.setVisibility(View.VISIBLE);
+            }
 
             LoadHistoryTaskParams loadHistoryTaskParams = new LoadHistoryTaskParams(
                     HistoryType.FoodServings,
@@ -103,8 +107,12 @@ public class ServingsHistoryActivity extends DailyDozenActivity
             return;
         }
 
-        binding.dailyServingsLoadingLabel.setVisibility(View.GONE);
-        binding.dailyServingsLoading.setVisibility(View.GONE);
+        if (binding.dailyServingsLoadingLabel != null) {
+            binding.dailyServingsLoadingLabel.setVisibility(View.GONE);
+        }
+        if (binding.dailyServingsLoading != null) {
+            binding.dailyServingsLoading.setVisibility(View.GONE);
+        }
         binding.dailyServingsChart.setVisibility(View.VISIBLE);
 
         binding.dailyServingsChart.setData(chartData);
